616e04747aa08936bd39ae87c21164963c6752e8
Matiss Vaisla <vmatiss@gmail.com>
2021-02-04 20:12:08 +0200

    missing js file for tabs

---
+++ wp-content/themes/sportapils/js/tabs.js
@@ -0,0 +1,56 @@
+/**
+ * File tabs.js.
+ *
+ * Handles toggling the tabs
+ * tabs support for dropdown menus.
+ */
+( function() {
+	// Get all the tabs elements within the tab navigation.
+	const listTabs = document.querySelectorAll(".list-nav > .list-tab");
+
+	// Return early if the tabs don't exist.
+	if ( ! listTabs ) {
+		return;
+	}
+
+	/**
+	 * Sets or removes .opened class on an element.
+	 */
+	function toggleClass(target) {
+	  const parent = target.parentElement; 
+	  if (parent.classList.contains('opened')) {
+	    parent.classList.remove('opened');
+	  } else {
+	    parent.classList.add('opened');  
+	  }
+	}
+	// Toggle the .active class each time the tab is clicked.
+	function listTabClicks(tabClickEvent) {
+	  const myContentPanes = document.querySelectorAll(".list-panel");
+	  const anchorReference = tabClickEvent.target;
+	  const activePaneId = anchorReference.getAttribute("href");
+	  const activePane = document.querySelector(activePaneId);
+	  const clickedTab = tabClickEvent.currentTarget;
+	  
+	  for (let i = 0; i < listTabs.length; i++) {
+	    listTabs[i].classList.remove("active");
+	  }
+
+	  clickedTab.classList.add("active");
+	
+	  toggleClass(clickedTab)
+	
+	  tabClickEvent.preventDefault();
+	
+	  for (let i = 0; i < myContentPanes.length; i++) {
+	    myContentPanes[i].classList.remove("active");
+	  }
+
+	  activePane.classList.add("active");
+	}
+
+	for (i = 0; i < listTabs.length; i++) {
+	  listTabs[i].addEventListener("click", listTabClicks)
+	}
+
+}() );
